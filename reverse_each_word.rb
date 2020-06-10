c5dd2b5dc5c658e2f34f48566436de19a37efa43
Daniel Hahm <dhahm01@gmail.com>
2020-06-10 19:00:33 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   output = ""
-  array.each do |element|
+  array.collect do |element|
     output += " " + element.reverse
   end 
   output.strip
